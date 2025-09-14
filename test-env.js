23578d40bdbaf9c85adfa7aa2d6288c129f2dfcd
sanjeev0303 <sanjeev.081021@gmail.com>
2025-09-14 08:09:30 +0530

    refactor: streamline worker scripts and remove obsolete test files

--- test-env.js
+++
@@ -1,5 +0,0 @@
-// Test environment variable reading
-console.log('Environment test:');
-console.log('USE_MOCK_RENDERING:', process.env.USE_MOCK_RENDERING);
-console.log('Parsed as boolean:', process.env.USE_MOCK_RENDERING === 'true');
-console.log('NODE_ENV:', process.env.NODE_ENV);
